88b032f811bd161663e7ce78fcdbbad3448ea1fe
Drew Weisenberg <weisenbergd@gmail.com>
2025-01-14 12:11:00 -0500

    add main margin

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -4,12 +4,12 @@ import NavBar from "./_components/Nav/NavBar";
 import Footer from "./_components/Foot/Footer";
 
 const Layout = ({ children }: LayoutProps) => {
-  // min-h-[calc(100vh-(var(--footerHeight)-var(--navHeight)))]
+  // main min-height: screen - margin - footerHeight - nav Height
 
   return (
     <div className="min-h-screen grid grid-rows-[1fr_auto]">
       <NavBar />
-      <main className="mt-6 md:mt-10 container px-1.5 mx-auto md:px-4 max-w-7xl min-h-[calc(100vh-1.5rem-(var(--footerHeight)+var(--navHeight)))] md:min-h-[calc(100vh-2.5rem-(var(--footerHeight)+var(--navHeight)))] ">
+      <main className="mt-6 mb-6 md:mt-10 container px-1.5 mx-auto md:px-4 max-w-7xl min-h-[calc(100vh-3rem-(var(--footerHeight)+var(--navHeight)))] md:min-h-[calc(100vh-4rem-(var(--footerHeight)+var(--navHeight)))]">
         {children}
       </main>
       <Footer />
